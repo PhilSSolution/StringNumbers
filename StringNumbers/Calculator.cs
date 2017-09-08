3f9ef91a6aad9a7a68da2617da9daa401c2570ce
Phillip Smit <psmit111@sig.ads>
2017-09-08 14:25:12 +0200

    adding StringNumbers

---
+++ StringNumbers/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringNumbers
+{
+    public class Calculator
+    {
+        public static int Add(string input)
+        {
+            
+            var sum = 0;
+            string[] delimiters;
+
+            if (input == "") return 0;
+
+            delimiters = Delimiters(ref input);
+
+            var listofnumbers = input.Split(delimiters, StringSplitOptions.None);
+            foreach (var number in listofnumbers)
+            {
+                try
+                {
+                   
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+                sum += Convert.ToInt32(number);
+            }
+            return sum;
+        }
+
+        private static string[] Delimiters(ref string input)
+        {
+            string[] delimiters;
+            if (input.StartsWith("//"))
+            {
+                delimiters = new string[] {input.Substring(2, 1)};
+                input = input.Substring(3);
+            }
+            else
+            {
+                delimiters = new string[] {",", "\n"};
+            }
+            return delimiters;
+        }
+    }
+}
---
+++ StringNumbers/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringNumbers
+{
+    [TestFixture]
+    public class Class1
+    {
+        
+
+        [TestCase("Negatives not allowed -1", "3,-1")]
+        [TestCase("Negatives not allowed -3 -1", "-3,-1")]
+        [TestCase(0, "")]
+        [TestCase(3, "3")]
+        [TestCase(12, "3,2,3,4")]
+        [TestCase(6, "1\n2,3")]
+        [TestCase(5, "//;\n2;3")]
+        [TestCase(4, "//c\n1c3")]
+        
+        [Test]
+        public static void addNumbers(int expected, string input)
+        {
+            var number = Calculator.Add(input);
+            Assert.AreEqual(expected, number);
+        }
+    }
+}
